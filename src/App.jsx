1fd4f3d73113f621aa6f3133b6538aa31fea9063
Anuj5504 <anujjadhav0505@gmail.com>
2025-02-22 11:07:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import { Button } from "./components/ui/button"
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
