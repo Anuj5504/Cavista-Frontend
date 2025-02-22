ddb782b0e86a5dba913f965e391fba7df6b73aef
prajwal07 <prajwal.nimbalkar1910@gmail.com>
2025-02-22 12:55:48 +0530

    dsad

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import { useState } from 'react'
 import Navbar from './ComponentsUI/Navbar'
 import Hero from './ComponentsUI/Hero'
+import HealthCare from './Pages/HealthCare';
+import {Routes,Route} from 'react-router-dom';
 function App() {
 
 
   return (
     <>
       <div>
-        <Hero/>
+        <Routes>
+          <Route path="/" element = {<Hero></Hero> } ></Route>
+          <Route path="/HealthCare" element = {<HealthCare></HealthCare> } ></Route>
+       
+        {/* <HealthCare></HealthCare> */}
+
+        </Routes>
       </div>
     </>
   )
